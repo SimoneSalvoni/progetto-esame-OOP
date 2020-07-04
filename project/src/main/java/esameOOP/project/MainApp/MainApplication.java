1da4f8a51dc4e4dcec24122a19ffe085fc6d0e6d
Simone Salvoni <simone.salvoni99@gmail.com>
2020-07-04 18:12:17 +0200

    Aggiunta la classe Feed, modificata la classe Post

--- project/src/main/java/esameOOP/project/MainApp/MainApplication.java
+++ project/src/main/java/esameOOP/project/MainApp/MainApplication.java
@@ -2,12 +2,22 @@ package esameOOP.project.MainApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jackson.JsonObjectDeserializer;
 
-@SpringBootApplication
+import esameOOP.project.Model.Feed;
+
+/**
+ * @SpringBootApplication public class MainApplication {
+ * 
+ *                        public static void main(String[] args) {
+ *                        SpringApplication.run(MainApplication.class, args); }
+ * 
+ *                        }
+ */
+
 public class MainApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(MainApplication.class, args);
+		Feed feed = new Feed();
+		System.out.println(feed.getJSONArray());
 	}
-
 }
