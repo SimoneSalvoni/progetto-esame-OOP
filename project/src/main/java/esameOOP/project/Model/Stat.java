6d7d90cfc275b561038f5564b2a5616f7bcd298b
Daniele Staffolani <danielestaffolani@libero.it>
2020-07-07 18:29:27 +0200

    Aggiunte le classi Stat, StatLengh, StatPolitic, StatTime.

--- project/src/main/java/esameOOP/project/Model/Stat.java
+++ project/src/main/java/esameOOP/project/Model/Stat.java
@@ -1,5 +1,39 @@
 package esameOOP.project.Model;
 
+import java.util.Vector;
+
 public class Stat {
+	private StatLenght StatLenght;
+	private StatPolitics StatPolitics;
+	private StatTime StatTime;
+	
+	public Stat(Vector<Post> feed) {
+		this.StatLenght = new StatLenght(feed);
+		this.StatPolitics = new StatPolitics(feed);
+		this.StatTime = new StatTime(feed);
+	}
+	
+	public StatLenght getStatLenght() {
+		return StatLenght;
+	}
+	public void setStatLenght(StatLenght statLenght) {
+		StatLenght = statLenght;
+	}
+	
+	public StatPolitics getStatPolitics() {
+		return StatPolitics;
+	}
+	public void setStatPolitics(StatPolitics statPolitics) {
+		StatPolitics = statPolitics;
+	}
+
+	public StatTime getStatTime() {
+		return StatTime;
+	}
+	public void setStatTime(StatTime statTime) {
+		StatTime = statTime;
+	}
+
 
 }
+
