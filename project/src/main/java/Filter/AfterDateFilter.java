395201ecdaf1c8134428db5d34449559c5621a68
Daniele Staffolani <danielestaffolani@libero.it>
2020-07-09 14:17:53 +0200

    Aggiunte le classi: Filter, DateFilter, MediaFilter, PoliticFilter, BetweenDatesFilter, AfterDatefilter, BeforeDateFilter

---
+++ project/src/main/java/Filter/AfterDateFilter.java
@@ -0,0 +1,20 @@
+package Filter;
+
+import java.util.Calendar;
+
+import esameOOP.project.Model.Post;
+
+public class AfterDateFilter extends DateFilter {
+	private Calendar date;
+	
+	public AfterDateFilter(String field, String operator, Calendar date) {
+		super(field, operator);
+		this.date=date;
+	}
+
+	@Override
+	public boolean CheckFilter(Post post) {
+		if (post.getCreated_time().compareTo(date) > 0) return true;  
+		else return false;
+	}
+}
