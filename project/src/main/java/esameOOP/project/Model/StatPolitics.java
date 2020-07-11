da24304d8f56a915589751343165a8d46e77591f
Daniele Staffolani <danielestaffolani@libero.it>
2020-07-11 18:16:05 +0200

    Modificate le classi StatLenght e StatPolitics

--- project/src/main/java/esameOOP/project/Model/StatPolitics.java
+++ project/src/main/java/esameOOP/project/Model/StatPolitics.java
@@ -5,59 +5,51 @@ import java.util.Vector;
 import esameOOP.project.Util.*;
 
 public class StatPolitics {
-	private double numPoliticPost;
-	private double numNationalPost;
-	private double numEUPost;
-	private double numExtraEUPost;
-	private double numInternationalPost;
+	private String numPoliticPost;
+	private String numNationalPost;
+	private String numEUPost;
+	private String numExtraEUPost;
+	private String numInternationalPost;
+	
 
-	public StatPolitics(Vector<Post> feed) {
+	public StatPolitics(Vector<Post> vector) {
 		double[] cont = new double[6];
-		cont = Calculate.countPolitic(feed);
-		this.numPoliticPost = cont[1];
-		this.numNationalPost = cont[2];
-		this.numEUPost = cont[3];
-		this.numExtraEUPost = cont[4];
-		this.numInternationalPost = cont[5];
+		cont = Calculate.countPolitic(vector);
+		this.numPoliticPost = cont[1]+"%";
+		this.numNationalPost = cont[2]+"%";
+		this.numEUPost = cont[3]+"%";
+		this.numExtraEUPost = cont[4]+"%";
+		this.numInternationalPost = cont[5]+"%";
 	}
 
-	public double getNumPoliticPost() {
+	public String getNumPoliticPost() {
 		return numPoliticPost;
 	}
-
-	public void setNumPoliticPost(double numPoliticPost) {
+	public void setNumPoliticPost(String numPoliticPost) {
 		this.numPoliticPost = numPoliticPost;
 	}
-
-	public double getNumNationalPost() {
+	public String getNumNationalPost() {
 		return numNationalPost;
 	}
-
-	public void setNumNationalPost(double numNationalPost) {
+	public void setNumNationalPost(String numNationalPost) {
 		this.numNationalPost = numNationalPost;
 	}
-
-	public double getNumEUPost() {
+	public String getNumEUPost() {
 		return numEUPost;
 	}
-
-	public void setNumEUPost(double numEUPost) {
+	public void setNumEUPost(String numEUPost) {
 		this.numEUPost = numEUPost;
 	}
-
-	public double getNumExtraEUPost() {
+	public String getNumExtraEUPost() {
 		return numExtraEUPost;
 	}
-
-	public void setNumExtraEUPost(double numExtraEUPost) {
+	public void setNumExtraEUPost(String numExtraEUPost) {
 		this.numExtraEUPost = numExtraEUPost;
 	}
-
-	public double getNumInternationalPost() {
+	public String getNumInternationalPost() {
 		return numInternationalPost;
 	}
-
-	public void setNumInternationalPost(double numInternationalPost) {
+	public void setNumInternationalPost(String numInternationalPost) {
 		this.numInternationalPost = numInternationalPost;
 	}
 
